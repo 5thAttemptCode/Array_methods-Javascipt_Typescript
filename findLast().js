bc300180d401792aefc3c1440fc2b4c870eed0b6
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-06-16 21:27:17 +0200

    Add files via upload

---
+++ findLast().js
@@ -0,0 +1,23 @@
+// findLast() - Returns the last element that passes a test.
+// Scenario: Continuing with the e-commerce inventory check scenario.
+// It wants to quickly find the last product that is out of stock to prioritize restocking efforts.
+
+
+const inventory = [
+  { product: "Breitling Chronomat", inStock: true },
+  { product: "Breitling Navtimer", inStock: true },
+  { product: "Breitling Avenger", inStock: false },
+  { product: "Breitling Top Time", inStock: false },
+  { product: "Breitling Superocean", inStock: true }
+]
+
+const outOfStockProduct = inventory.findLast(item => !item.inStock)
+
+if (outOfStockProduct) {
+  console.log(`The last out-of-stock product is: ${outOfStockProduct.product}`)
+} else {
+  console.log("All products are in stock.")
+}
+
+// Result
+// "The last out-of-stock product is: Breitling Top Time"
