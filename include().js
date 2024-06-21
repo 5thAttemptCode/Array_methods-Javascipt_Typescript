2803d6668246ade58b709f917137fc0742e1086f
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-06-21 23:43:57 +0200

    Add files via upload

---
+++ include().js
@@ -0,0 +1,16 @@
+// include() - Executes a function on each element.
+// Scenario: Imagine you are managing a promotion system for an online store. You have a list of products that 
+// are currently on promotion, and you want to check if a specific product is included in that list.
+
+
+const promotionList = ['prod1', 'prod2', 'prod3', 'prod4']
+
+const productToCheck = 'prod3'
+
+if (promotionList.includes(productToCheck)) {
+  console.log(`Product ${productToCheck} is on promotion`)
+} else {
+  console.log(`Product ${productToCheck} is not on promotion`)
+}
+// Result:
+// "Product prod3 is on promotion"
