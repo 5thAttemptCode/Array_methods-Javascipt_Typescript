1053fadd409bd26b5f0db220029adc9b0ef2a73a
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-08-15 09:11:26 +0200

    Add files via upload

---
+++ push().ts
@@ -0,0 +1,17 @@
+// push() - Adds one or more elements to the end.
+// Scenario: Useful for a shopping cart system in e-commerce.
+// Users can add items to their cart, and you want to handle this action using the push() method.
+
+
+let shoppingCart: string[] = ["Laptop", "Mouse", "Keyboard"]
+
+const newItem: string = "Monitor"
+
+shoppingCart.push(newItem)
+
+console.log(`Added "${newItem}" to the shopping cart.`)
+console.log("Updated shopping cart:", shoppingCart)
+
+// Result:
+// "Added "Monitor" to the shopping cart."
+// Updated shopping cart: ['Laptop', 'Mouse', 'Keyboard', 'Monitor']
