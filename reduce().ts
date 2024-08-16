b93bfb012598fcd97624d326c193448ab5043ec0
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-08-16 09:48:26 +0200

    Add files via upload

---
+++ reduce().ts
@@ -0,0 +1,13 @@
+// reduce() - Applies a function against an accumulator and each element to reduce it to a single value.
+// Scenario: Useful for a shopping cart system in e-commerce
+// to calculate the cart total cost.
+
+
+const itemPrices: number[] = [29.99, 49.99, 19.99, 99.99]
+
+const totalCost: number = itemPrices.reduce((accumulator: number, currentValue: number) => accumulator + currentValue, 0)
+
+console.log(`Total price to pay: $${totalCost.toFixed(2)}`)
+
+// Result:
+// "Total cost of items in the shopping cart: $199.96"
