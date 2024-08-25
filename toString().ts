312938186b6cbff0681a7fe16afaded992c0ead7
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-08-25 04:26:20 +0200

    Add files via upload

---
+++ toString().ts
@@ -0,0 +1,18 @@
+// toLocaleString() - Returns a string representing the array.
+// Scenario: An online store wants to send a marketing email that includes a list of popular product names, all in a single string.
+
+
+const popularProducts: string[] = [
+  "HUAWEI MatePad T 10s",
+  "Apple iPad Pro",
+  "Samsung Galaxy Tab S7",
+  "Amazon Fire HD 10",
+  "Microsoft Surface Pro 7"
+]
+
+const productNamesString: string = popularProducts.toString()
+
+console.log("Popular products for the marketing email:", productNamesString)
+
+// Result:
+// Popular products for the marketing email: HUAWEI MatePad T 10s,Apple iPad Pro,Samsung Galaxy Tab S7,Amazon Fire HD 10,Microsoft Surface Pro 7
